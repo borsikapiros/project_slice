b3e2e12b20de6d265d807c26de3edf49a443c0de
borsikapiros <73432795+borsikapiros@users.noreply.github.com>
2020-12-10 09:30:51 +0100

    comments added

---
+++ script.js
@@ -0,0 +1,33 @@
+//todo app, 10 üres mező default + lehet újat hozzáadni, checkbox + text input mező, törlés gomb minden sornál
+
+function pageLoad {
+    // változót létrehozni a root-nak
+
+
+    // eltárolni a default számot másik változóban
+
+
+    // div, checkbox, input mező és törlés button egy többsoros változóban (egy sor html struktúrája, ` jellel)
+
+
+    // ezt 10x létre kell hozni egy for ciklussal és hozzáadni a felülethez
+
+
+    // plusz gomb lista végére, vagy utána (afterend)
+
+
+    // clickkel hozzáadunk egy újabb sort a root elemhez
+
+
+    // querySAll-lal classnév alapján a törlés gombokat kijelölöm és elmentem változóba
+
+
+    // for ciklussal ráteszünk mindre click eseményt 
+
+
+    // hogy jelölöm ki az elemet, aminek a parentjének kitöröljük az outerhtml-jét
+    
+
+}
+
+window.addEventListener('pageLoad', load);
